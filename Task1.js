4ed2774c36b5c4382fafbe74407fd6af17da43a0
DineshPandian26 <117209943+DineshPandian26@users.noreply.github.com>
2026-08-27 23:55:03 +0530

    Add files via upload

---
+++ Task1.js
@@ -0,0 +1,16 @@
+let stud_name = "Dinesh";
+let age = 22;
+let mark = 100;
+
+console.log(stud_name,age,mark)
+
+let user_name = prompt("What is your name");
+alert(user_name);
+
+let user_age = prompt("What is your age");
+console.log(user_age);
+
+confirm("Is your age 22?");
+
+var user= prompt("What is your name");
+document.writeln(user);
