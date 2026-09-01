19139400c5d6339cd55dd000a0d89cecbfbb5556
DineshPandian26 <117209943+DineshPandian26@users.noreply.github.com>
2026-09-01 18:42:41 +0530

    Add files via upload

---
+++ Task2.js
@@ -0,0 +1,241 @@
+//Task 1
+
+// var name = "dinesh";
+// let age =21 ;
+// const clg = "Mepco";
+
+// console.log(name);
+// console.log(age);
+// console.log(clg);
+
+// document.writeln(name);
+// document.writeln(age);
+// document.writeln(clg);
+
+// name= "pranesh"
+// console.log(name)
+
+// age = 16
+// console.log(age);
+
+// // clg="velammal"
+
+// var name ="Kavi"
+// console.log(name)
+
+// let age = 22
+
+
+
+// Task2
+
+// var name = prompt("Enter your name");
+// console.log(name);
+// let age = prompt("Enter your age");
+// console.log(age);
+// let city= prompt("Enter your city");
+// console.log(city);
+
+
+// Task3
+
+// var name = prompt("Enter your name");
+// alert("Welcome "+ name);
+
+// Task4
+
+// let year = prompt("Enter your birth year");
+// console.log(2026-year);
+
+
+// Task5
+
+// let name = "hello"
+// console.log(typeof(name));
+// let num = 123
+// console.log(typeof(num))
+// let num1 = 12.4
+// console.log(typeof(num1))
+// let num3= true;
+// console.log(typeof(num3))
+// let empty
+// console.log(typeof(empty));
+// let check =prompt()
+// console.log(typeof(check));
+
+
+
+// Task6
+
+// let student={
+//     name:"Dinesh",
+//     age:21,
+//     city:"madurai",
+//     qualification: "BE",
+//     isStudent: true
+// }
+// console.log(student);
+// console.log(student.name);
+// console.log(student.age);
+// console.log(student.city);
+// console.log(student.qualification);
+// console.log(student.isStudent);
+
+
+// Task7
+
+// let fruits=["Apple","Mango","Orange","Banana","Grapes","Papaya"]
+// console.log(fruits[0])
+// console.log(fruits[1])
+// console.log(fruits[fruits.length-1])
+// console.log(fruits.length);
+
+
+
+// Task8
+
+// let a= 10
+// let b= 5
+
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a/b)
+// console.log(a%b)
+// console.log(a*b)
+// console.log(a**b)
+
+
+// Task9
+
+// let shirt =999
+// let pant = 1499
+// let shoes=1999
+
+// console.log(shirt+pant+shoes)
+
+
+// Task10
+
+// let tamil= 80
+// let english = 75
+// let maths = 90
+
+// console.log(tamil+english+maths)
+
+// Task11
+
+// let a = 10;
+
+// let b = a++;
+
+// console.log(a);  //11
+// console.log(b);  //10
+
+
+// Task12
+
+// let a = 10;
+
+// let b = ++a;
+
+// console.log(a);   // 11
+// console.log(b);   // 11
+
+
+// Task13
+
+// let a = 20;
+
+// let b = a--;
+
+// console.log(a); // 19
+// console.log(b); // 20
+
+
+// Task14
+
+// let a = 20;
+
+// let b = --a;
+
+// console.log(a);  // 19
+// console.log(b);  // 19
+
+
+// Task15
+
+// let a = 5;
+
+// let b = a++;
+
+// let c = ++a;
+
+// let d = b--;
+
+// console.log(a);  // 7
+// console.log(b);  // 4
+// console.log(c);  // 7
+// console.log(d);  // 5
+
+// Task16
+
+// let num = 10;
+// num +=5
+// console.log(num)
+// num -=5
+// console.log(num)
+
+// num *= 5;
+// console.log(num)
+
+// num /= 5;
+// console.log(num)
+
+// num **= 5
+// console.log(num)
+
+// num %= 5
+// console.log(num)
+
+
+
+// Task17
+
+// let student ={
+//  name : "Dinesh",
+//  age  : 21,
+//  city : "madurai",
+//  clg  : "mepco",
+//  sub  : ["tam","eng","mat","science","social"]
+// }
+
+// let n=student.sub.length
+// console.log(student)
+// console.log(student.name)
+// console.log(age)
+// console.log(student.city)
+// console.log(student.sub[0])
+// console.log(student.sub[n-1])
+// console.log(n)
+
+ 
+
+
+// Final Task
+
+
+// let a= prompt("Enter first number")
+// let b= prompt("Enter second number")
+
+// let add = a+b
+// console.log(add)
+// let sub = a-b;
+// console.log(sub);
+// let mul = a*b;
+// console.log(mul)
+// let div = a/b;
+// console.log(div)
+// let mod = a%b;
+// console.log(mod)
+// let expo= a**b;
+// console.log(expo);
